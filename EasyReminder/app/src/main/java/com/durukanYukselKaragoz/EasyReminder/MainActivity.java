4384489bfd906b22e1c28f13600f1e7ce6392977
E. Berke KARAGÖZ <34271483+BerkeKaragoz@users.noreply.github.com>
2020-01-05 02:16:09 +0300

    Progress Theme

--- EasyReminder/app/src/main/java/com/durukanYukselKaragoz/EasyReminder/MainActivity.java
+++ EasyReminder/app/src/main/java/com/durukanYukselKaragoz/EasyReminder/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
         //Hide notification bar
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
